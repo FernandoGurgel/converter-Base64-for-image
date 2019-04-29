44d2771669fad16c9db53e87a1b5000ec6af8d97
Fernando Gurgel <fernando.gurgel@ipdec.org>
2019-04-29 18:21:33 -0400

    init convert list base64 with file json

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
-// var base64ToImage = require('base64-to-image');
-var json = require('./images.json');
+const fs = require('fs');
+const base64ToImage = require('base64-to-image');
 
-var obj = JSON.stringify(json);
+let rawdata = fs.readFileSync('images.json');
+let images = JSON.parse(rawdata);
+
+// convertendo base64 para image
+// images.forEach( data => {
+//     var path = 'images';
+//     var optionalObj = {'fileName': data.id , 'type':'jpeg'};
+
+//     var status = base64ToImage(data.str_photo,path,optionalObj);
+
+//     console.log(status);
+// });
 
 
-
-console.log(json[0]);
+images.forEach(element => {
+    if (element.id == '1150'){
+    
+        console.log(element.str_photo)
+    }
+});
